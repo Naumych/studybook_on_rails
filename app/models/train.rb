14b5d3ff5be2219e5ba20f0402b5f99ed7661e0d
Kseniya Naumycheva <naumycheva@naumycheva-lx.nixsolutions.com>
2019-08-16 18:57:27 +0300

    use method where

--- app/models/train.rb
+++ app/models/train.rb
@@ -7,20 +7,4 @@ class Train < ApplicationRecord
   belongs_to :route
   has_many :tickets
   has_many :carriages
-
-  def carriages_depending_on_type(carriage_type)
-    carriages.select { |carriage| carriage.carriage_type == carriage_type }
-  end
-
-  def carriages_number_depending_on_type(carriage_type)
-    carriages_depending_on_type(carriage_type).size
-  end
-
-  def top_seats_count(carriage_type)
-    carriages_depending_on_type(carriage_type).sum(&:top_seats_count)
-  end
-
-  def lower_seats_count(carriage_type)
-    carriages_depending_on_type(carriage_type).sum(&:lower_seats_count)
-  end
 end
