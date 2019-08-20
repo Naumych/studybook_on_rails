3e1f5d496f86b7b6ea052706d3bf2d22fc7bdace
Kseniya Naumycheva <naumycheva@naumycheva-lx.nixsolutions.com>
2019-08-20 15:29:26 +0300

    present carriage types as a constant

--- app/models/carriage.rb
+++ app/models/carriage.rb
@@ -4,7 +4,9 @@ class Carriage < ApplicationRecord
   validates :carriage_type, presence: true
   belongs_to :train
 
+  CARRIAGE_TYPES = %w[compartment econom-class].freeze
+
   def self.carriage_types
-    %w[compartment econom-class]
+    CARRIAGE_TYPES
   end
 end
