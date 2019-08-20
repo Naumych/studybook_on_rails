9e72f6e51f2136c3005e101a636aea33ef1e8801
Kseniya Naumycheva <naumycheva@naumycheva-lx.nixsolutions.com>
2019-08-20 21:56:38 +0300

    add_position not working

--- app/models/railway_stations_route.rb
+++ app/models/railway_stations_route.rb
@@ -5,4 +5,29 @@ class RailwayStationsRoute < ApplicationRecord
   belongs_to :route
 
   validates :railway_station_id, uniqueness: { scope: :route_id }
+  # validates :position, uniqueness: { scope: :route_id }
+
+  after_create :add_position
+
+  # private
+
+  # def add_position
+  #   select('railway_stations').where(route_id: route_id)
+  # end
+
+  def add_position
+    if route.railway_stations_routes.where(position: true).present?
+      binding.pry
+
+      max_position = route.railway_stations_routes.map(&:position).max
+      update(position: max_position + 1)
+    else
+      binding.pry
+      route.railway_stations_routes.each_with_index do |station, index|
+        binding.pry
+        station.update(position: index + 1)
+      end
+    end
+  end
 end
+# route.railway_stations_routes.where(position: true).
--- app/models/route.rb
+++ app/models/route.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class Route < ApplicationRecord
-  has_many :railway_stations_routes
+  has_many :railway_stations_routes, dependent: :destroy
   has_many :railway_stations, through: :railway_stations_routes
   has_many :trains
 
@@ -9,6 +9,7 @@ class Route < ApplicationRecord
   validate :stations_count
 
   before_validation :set_name
+  # after_save :add_position
 
   private
 
@@ -19,4 +20,13 @@ class Route < ApplicationRecord
   def stations_count
     errors.add(:base, 'Route should contain at least 2 stations') if railway_stations.size < 2
   end
+
+  # def add_position
+  #   railway_stations = RailwayStationsRoute.where(route_id: id, position: true)
+  #   railway_stations_2 = RailwayStationsRoute.where(route_id: id, position: nil)
+  #   railway_stations_2.each_with_index do |rsr, index|
+  #     rsr.update(position: index + railway_stations.size)
+  #     binding.pry
+  #   end
+  # end
 end
