b472dbb11ef1037d5a3253e49a1ee761298296ea
Kseniya Naumycheva <naumycheva@naumycheva-lx.nixsolutions.com>
2019-08-23 15:53:25 +0300

    fix train_carriages_info with all types, add validate to all carriages, fix checkbox for head/tail ordered

--- db/migrate/20190819154159_create_carriages.rb
+++ db/migrate/20190819154159_create_carriages.rb
@@ -3,11 +3,11 @@ class CreateCarriages < ActiveRecord::Migration[5.2]
     create_table :carriages do |t|
       t.string :type
       t.integer :number
-      t.integer :top_seats
-      t.integer :bottom_seats
-      t.integer :side_top_seats
-      t.integer :side_bottom_seats
-      t.integer :seats
+      t.integer :top_seats, default: 0
+      t.integer :bottom_seats, default: 0
+      t.integer :side_top_seats, default: 0
+      t.integer :side_bottom_seats, default: 0
+      t.integer :seats, default: 0
       t.belongs_to :train
       t.integer :number_in_train
 
