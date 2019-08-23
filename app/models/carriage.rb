13590293466ef40a8f1e06828d5563f29a522e86
Kseniya Naumycheva <naumycheva@naumycheva-lx.nixsolutions.com>
2019-08-23 17:53:21 +0300

    fix add_number_in_train

--- app/models/carriage.rb
+++ app/models/carriage.rb
@@ -7,19 +7,20 @@ class Carriage < ApplicationRecord
 
   after_create :add_number_in_train
 
+  CARRIAGE_TYPES = %w[EconomyCarriage CoupeCarriage SVCarriage SeatedCarriage].freeze
+
   def self.carriage_types
-    %w[EconomyCarriage CoupeCarriage SVCarriage SeatedCarriage]
+    CARRIAGE_TYPES
   end
 
   def add_number_in_train
     carriages_in_train = Carriage.where(train_id: train_id)
     number_in_train_presented = carriages_in_train.select { |carriage| carriage.number_in_train.present? }
-
-    self.number_in_train = if number_in_train_presented.present?
-                             number_in_train_presented.map(&:number_in_train).max + 1
-                           else
-                             1
-                           end
-    save
+    if number_in_train_presented.present?
+      number_in_train_to_update = number_in_train_presented.map(&:number_in_train).max + 1
+      update(number_in_train: number_in_train_to_update)
+    else
+      update(number_in_train: 1)
+    end
   end
 end
